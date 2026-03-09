1db8ced065c0a6d4cc4581f77cfc4607aa780524
何永康 <heyk@xlight.cn>
2026-03-09 15:52:20 +0800

    更新脑图

---
+++ 2026/ajax.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * @param {*} options
+ */
+const ajax = (options) => {
+  let timer = null;
+  // 处理post和get的参数
+  const setParam = (data) => {
+    let params = [];
+    for (let key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        params.push(
+          `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`,
+        );
+      }
+    }
+    params.push(`t=${new Date().getTime()}`);
+    return params.join("&");
+  };
+
+  // 1、创建XHR对象
+  let xhr;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  const params = setParam(options.data);
+  // 打开连接
+  if (options.method.toLowerCase() === "get") {
+    xhr.open(options.method, `${options.url}?${params}`, true);
+    xhr.send();
+  } else if (options.method.toLowerCase() === "post") {
+    xhr.open(options.method, options.url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(params);
+  }
+
+  // 监听变化
+  xhr.onreadystatechange = function () {
+    // 判断readyState
+    if (xhr.readyState === 4) {
+      if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+        options.success && options.success(xhr.responseText);
+        clearTimeout(timer);
+      } else {
+        options.error && options.error(xhr.responseText);
+      }
+    }
+  };
+
+  // 判断有没有超时
+  if (options.timeout) {
+    timer = setTimeout(() => {
+      xhr.abort();
+      clearTimeout(timer);
+    }, options.timeout);
+  }
+};
--- 2026/demo.js
+++ 2026/demo.js
@@ -1,11 +1,15 @@
-function Parent() {
-  this.name = "parent";
+function myBind() {
+  const context = [...arguments][0];
+  const args = [...arguments].slice(1);
+  const fn = this;
+  function fNOP() {}
+  const boundFn = function () {
+    const newArgs = [...arguments, ...args];
+    return fn.apply(this instanceof fNOP ? this : context, newArgs);
+  };
+
+  fNOP.prototype = this.prototype;
+  boundFn.prototype = new fNOP();
+
+  return boundFn;
 }
-
-Parent.prototype.getName = function () {
-  console.log(this.name);
-};
-
-function Child() {}
-
-Child.prototype = new Parent();
